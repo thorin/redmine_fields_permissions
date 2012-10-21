656ccb7814a8bee19b90b16c022fd8e9ad7fa329
Thorin <thorin.iv@gmail.com>
2012-10-21 22:32:55 +0200

    Control fields permissions redefining safe attributes

--- init.rb
+++ init.rb
@@ -8,19 +8,46 @@ Redmine::Plugin.register :redmine_fields_permissions do
   url 'http://www.sysdream.com/'
   author_url 'http://www.sysdream.com/'
 
-  # Permissions for assigned to field
-  permission :edit_assigned_to, :issues => :index
-  
-  # Permissions for due date field
-  permission :edit_due_date, :issues => :index
-  
-  # Permissions for estimated hours field
-  permission :edit_estimated_hours, :issues => :index
+  project_module :issue_tracking do
+    permission :edit_assigned_to, {}, :require => :member
+    permission :edit_start_date, {}, :require => :member
+    permission :edit_due_date, {}, :require => :member
+    permission :edit_estimated_hours, {}, :require => :member 
+    permission :edit_priority, {}, :require => :member 
+    permission :edit_fixed_version, {}, :require => :member 
+    permission :edit_category, {}, :require => :member 
+  end
 
-  # Permissions for priority field
-  permission :edit_priority, :issues => :index
+  ActionDispatch::Callbacks.to_prepare do
+    FIELDS = %w( assigned_to_id start_date due_date estimated_hours done_ratio priority_id fixed_version_id category_id )
+    Issue.safe_attributes.each {|attrs, options| attrs.delete_if(&FIELDS.method(:include?)) }
 
-  # Permissions for fixed version field
-  permission :edit_fixed_version, :issues => :index
+    Issue.safe_attributes 'assigned_to_id',
+      :if => lambda {|issue, user| user.allowed_to?(:edit_assigned_to, issue.project) && (user.allowed_to?(:edit_issues, issue.project) || issue.new_record?) }
+
+    Issue.safe_attributes 'start_date',
+      :if => lambda {|issue, user| user.allowed_to?(:edit_start_date, issue.project) && (user.allowed_to?(:edit_issues, issue.project) || issue.new_record?) }
+
+    Issue.safe_attributes 'due_date',
+      :if => lambda {|issue, user| user.allowed_to?(:edit_due_date, issue.project) && (user.allowed_to?(:edit_issues, issue.project) || issue.new_record?) }
+
+    Issue.safe_attributes 'estimated_hours',
+      'done_ratio',
+      :if => lambda {|issue, user| user.allowed_to?(:edit_estimated_hours, issue.project) && (user.allowed_to?(:edit_issues, issue.project) || issue.new_record?) }
+
+    Issue.safe_attributes 'priority_id',
+      :if => lambda {|issue, user| user.allowed_to?(:edit_priority, issue.project) && (user.allowed_to?(:edit_issues, issue.project) || issue.new_record?) }
+
+    Issue.safe_attributes 'fixed_version_id',
+      :if => lambda {|issue, user| user.allowed_to?(:edit_fixed_version, issue.project) && (user.allowed_to?(:edit_issues, issue.project) || issue.new_record?) }
+
+    Issue.safe_attributes 'category_id',
+      :if => lambda {|issue, user| user.allowed_to?(:edit_category, issue.project) && (user.allowed_to?(:edit_issues, issue.project) || issue.new_record?) }
+
+    Issue.safe_attributes 'assigned_to_id',
+      :if => lambda {|issue, user| issue.new_statuses_allowed_to(user).any? && user.allowed_to?(:edit_assigned_to, issue.project) }
+
+    Issue.safe_attributes 'fixed_version_id',
+      :if => lambda {|issue, user| issue.new_statuses_allowed_to(user).any? && user.allowed_to?(:edit_fixed_version, issue.project) }
+  end
 end
-
